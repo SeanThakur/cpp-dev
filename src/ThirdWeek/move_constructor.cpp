6d26f9ebc1f498c528702b5e521374580fb3a5df
SeanThakur <vikash_thakur@amperehourenergy.com>
2026-04-16 14:53:42 +0530

    added move assignment operator for third week

--- src/ThirdWeek/move_constructor.cpp
+++ src/ThirdWeek/move_constructor.cpp
@@ -16,11 +16,32 @@ public:
         ptr = new int(*(source.ptr));
         std::cout << "Copy constructor called Deep copied the value " << *ptr << " To the new address " << ptr << "\n";
     }
+    // Move constructor
     HeapInt(HeapInt &&source) noexcept
     {
         this->ptr = source.ptr; // steal the resource
         std::cout << "Move constructor called " << "Stealing the address " << ptr << "\n";
         source.ptr = nullptr; // null out the source pointer
+    }
+    // move assignment operator
+    HeapInt &operator=(HeapInt &&source) noexcept
+    {
+        std::cout << "Move assignment operator called " << "\n";
+        if (this == &source)
+        {
+            return *this; // self-assignment check
+        }
+        // Free existing resource
+        if (this->ptr != nullptr)
+        {
+            std::cout << "Freeing existing resource at address " << this->ptr << "\n";
+            delete this->ptr;
+        }
+        // Steal the resource
+        this->ptr = source.ptr;
+        std::cout << "-> Stealing the address " << ptr << "\n";
+        source.ptr = nullptr; // null out the source pointer
+        return *this;
     }
     ~HeapInt()
     {
@@ -43,5 +64,6 @@ int main()
     HeapInt c(20);
     HeapInt d = std::move(c); // move constructor
     std::cout << "Value of d the move constructor that steal the c data: " << *d.ptr << " and the address " << d.ptr << "\n";
+    a = std::move(d); // move assignment operator
     return 0;
 }
